07dddbb0c25430e80ac2bdd649122a01afce251a
nithin <nithin.gunnu@gmail.com>
2020-03-06 10:30:24 +0530

    added expenses

--- js/app.js
+++ js/app.js
@@ -1,23 +1,45 @@
-////variable////
+////classes///
 
-///classes///
+class Budget {
+  constructor(budget) {
+    this.budget = Number(budget);
+    this.budgetleft = this.budget;
+  }
+}
 
-const addExpenseForm = document.querySelector("#add-expense");
+class HTML {
+  insertBudget(amount) {
+    hmtl.insertBudget(budget.budget);
+    budgetTotal.innerHTML = `${amount}`;
+    budgetLeft.innerHTML = `${amount}`;
+  }
+}
+
+////variables///
+
+const addExpenseForm = document.querySelector("#expense-form");
+budgetTotal = document.querySelector("span#total");
+budgetleft = document.querySelector("span#left");
 
 let budget, userBudget;
 
-eventliseners();
+/////eventlistners///
 
-function eventliseners() {
+eventlistners();
+
+function eventlistners() {
   document.addEventListener("DOMContentLoaded", function() {
     userBudget = prompt("what is your budget?");
-
     if (userBudget === "null" || userBudget === "" || userBudget === "0") {
       window.location.reload();
+
+      budget = new Budget(userBudget);
+      html = new Html();
+      html.insertBudget(budget);
     }
   });
 
-  addExpenseForm.eventliseners("submit", function(e) {
+  addExpenseForm.addEventListener("submit", function(e) {
     e.preventDefault();
   });
 }
